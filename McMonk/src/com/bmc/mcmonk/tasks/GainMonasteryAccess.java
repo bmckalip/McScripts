460d85bcab28b8626a77608e724eefcefe2a4483
bmckalip <brmckalip@gmail.com>
2018-04-28 14:12:39 -0400

    cleanup

--- McMonk/src/com/bmc/mcmonk/tasks/GainMonasteryAccess.java
+++ McMonk/src/com/bmc/mcmonk/tasks/GainMonasteryAccess.java
@@ -2,20 +2,14 @@ package com.bmc.mcmonk.tasks;
 
 import com.bmc.mclib.script.McScript;
 import com.bmc.mclib.tasks.Task;
-import org.dreambot.api.wrappers.interactive.NPC;
+
+import java.util.Random;
 
 public class GainMonasteryAccess extends Task {
-    private NPC abbot;
-    private String continueMsg = "Click here to continue";
-
-    public GainMonasteryAccess(McScript s) {
-        super(s);
-    }
+    public GainMonasteryAccess(McScript s) { super(s); }
 
     @Override
-    public boolean validate() {
-        return s.getDialogues().inDialogue();
-    }
+    public boolean validate() { return s.getDialogues().inDialogue(); }
 
     @Override
     public int execute() {
@@ -25,7 +19,7 @@ public class GainMonasteryAccess extends Task {
         }else if(s.getDialogues().canEnterInput()){
             s.getDialogues().chooseOption(1);
         }
-        return 200;
+        return new Random().nextInt(100) + 150;
     }
 
     @Override
